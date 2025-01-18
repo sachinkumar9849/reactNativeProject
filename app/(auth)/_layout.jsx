d9748ad6ae9966b9bd699ebc138eccdae08d9561
sachinkumar9849 <englishjha@gmail.com>
2025-01-18 14:38:50 +0545

    tailwind css install

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+
+const AuthLayout = () => {
+  return (
+    <>
+     
+      
+    </>
+  );
+};
+
+export default AuthLayout;
+
+const styles = StyleSheet.create({});
