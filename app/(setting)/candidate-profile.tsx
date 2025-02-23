2bc29370883c300b6268b527da0a993a5c2a31f6
sachinkumar9849 <englishjha@gmail.com>
2025-02-23 13:12:12 +0545

    candidate profile page layout done

--- app/(setting)/candidate-profile.tsx
+++ app/(setting)/candidate-profile.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { images } from '@/constants'
 
 const CandidateProfile = () => {
   return (
     <View>
-      <Text>CandidateProfile</Text>
+      <View className='px-4 flex justify-between flex-row py-8 border-b border-[#E6E6E6]'>
+        <Image source={images.angle}/>
+        <Text className='text-[#0A0A0B] text-[18px] font-semibold'>Profile</Text>
+        <Text></Text>
+      </View>
+      <View className='p-4 flex flex-row'>
+        <View className='mr-3'>
+          <Image source={images.settingUser}/>
+        </View>
+        <View>
+          <Text className='text-[14px] mb-4'>Cameron Williamson</Text>
+          <Text className='text-[12px] text-[#5A5555]'>Email</Text>
+          <Text className='text-[14px] mb-4'>cameronwilliamson@gmail.com</Text>
+
+          <Text className='text-[12px] text-[#5A5555]'>Address</Text>
+          <Text className='text-[14px] mb-4'>Croatia, Zagreb</Text>
+         
+        </View>
+      </View>
     </View>
   )
 }
--- constants/images.tsx
+++ constants/images.tsx
@@ -33,7 +33,8 @@ import support from "@/assets/images/setting/support.png";
 import bills from "@/assets/images/setting/bills.png";
 import arrowIcon from "@/assets/images/setting/arrow.png";
 import logout from "@/assets/images/setting/logout.png";
+import settingUser from "@/assets/images/setting/user.png";
 
 
 
-export default {logout,arrowIcon,bills,support,password,certificationsSetting,subscriptionSetting,profile,angle,addArrow,addExperience,role,close,add,skills1,arrowBlack,message,home,planning3,planning2,planning1,quiz,users,startImg ,Login,Arrow,LoginVerify,verifiedEmail,phoneVerify,banner,user,notice,logoSmall,completeProfile,search};
+export default {settingUser,logout,arrowIcon,bills,support,password,certificationsSetting,subscriptionSetting,profile,angle,addArrow,addExperience,role,close,add,skills1,arrowBlack,message,home,planning3,planning2,planning1,quiz,users,startImg ,Login,Arrow,LoginVerify,verifiedEmail,phoneVerify,banner,user,notice,logoSmall,completeProfile,search};
